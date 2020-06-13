3b6b88fee29233157a0108a260631913a391b813
Austin Carrig <austincarrig.raspi@gmail.com>
2020-06-13 13:24:20 -0400

    removed lock and console logging

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 import wasmInit from "./pkg/exports.js";
 import * as wasm from "./pkg/exports.js";
 
-let lock = true
-
 const runWasm = async () => {
   // Instantiate our wasm module
   const rustWasm = await wasmInit("./pkg/exports_bg.wasm");
 
   // Call the Add function export from wasm, save the result
   let result = wasm.call_me_from_javascript();
-
-  console.log(result.get_v()); // Should output '72'
-
-  lock = false
 };
 runWasm();
 
